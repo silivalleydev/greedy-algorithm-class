914bf79ef0f1c98604439f86180d016c34f39d23
silivalleydev <silivalleydev@gmail.com>
2022-05-18 20:53:47 +0900

    until it become one

--- numberCard.js
+++ numberCard.js
@@ -12,10 +12,22 @@ prompt.start();
  * 
  * 4. 따라서 처음에 카드를 골라낼 행을 선택할때 이후에 해당 행에서 가장 숫자가 낮은것을
  * 고려하여 최종적으로 가장 높은 숫자의 카드를 뽑을 수 있도록 전략을 세워야한다.
+ * 
+ * 입력
+ * 3 3
+ * 3 1 2
+ * 4 1 4
+ * 2 2 2
+ * 
+ * 정답 2
+ * 
+ * 입력
+ * 2 4
+ * 7 3 1 8
+ * 3 3 3 4
  */
  prompt.get(["rowNcol"], function(err, firstRes){
     const { rowNcol } = firstRes;
-    let answer = 0;
 
     function spaceStrToArray (str) {
         return str.split(" ");
